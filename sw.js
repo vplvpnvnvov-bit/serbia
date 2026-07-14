05c1a237c8c7bb65175e99e0d3ba8a8220260eaa
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-15 00:43:01 +0300

    chore: bump version to v1.33.14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v1.33.13-0950c9c';
+const CACHE_NAME = 'relocation-v1.33.14-5a0601a';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
