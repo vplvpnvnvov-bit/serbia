3978db3b8a5e5d93e9a21061b18a91fa274fe247
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-16 12:58:04 +0300

    chore: bump version to v1.78.3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  VERSION: "1.78.2",
-  BUILD: "21a24ef",
-  CACHE_NAME: "relocation-v1.78.2-21a24ef"
+  VERSION: "1.78.3",
+  BUILD: "1648bc9",
+  CACHE_NAME: "relocation-v1.78.3-1648bc9"
 };
 
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v1.78.2-21a24ef';
+const CACHE_NAME = 'relocation-v1.78.3-1648bc9';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
