756f8a0b0fb40a331bd4b5bc23c8b2af81302e08
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-17 00:23:55 +0300

    feat: enrich tips with bank, apostille, rental, kindergarten, license nuances

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v6.1.0-c243d7c';
+const CACHE_NAME = 'relocation-v6.2.0-1d24af0';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
