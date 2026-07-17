1f876ad2803c92eedc6da572940ea772273f0a10
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-17 20:18:16 +0300

    fix: обновлены дефолтные координаты ландшафта (грибы + берёза)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v6.12.4-07597de';
+const CACHE_NAME = 'relocation-v6.12.5-05b9867';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
