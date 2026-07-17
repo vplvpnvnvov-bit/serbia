5fd41ee5339a237cad89efbfa46b31e22eb6a18c
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-17 20:49:37 +0300

    fix: обновлены координаты вторички и ландшафта

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v6.13.1-efa3d32';
+const CACHE_NAME = 'relocation-v6.13.2-5767c0e';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
