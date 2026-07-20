58b4cbedeab4e94f8aa020376c86960de0dc9884
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-20 20:51:56 +0300

    feat: add 'close loans' task to plan and schema

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v6.31.1-ebfe549';
+const CACHE_NAME = 'relocation-v6.31.2-e330e4d';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
