07244193469d5faf86d918280bf7e307f8aec92e
vplvpnvnvov <vplvpnvnvov@gmail.com>
2026-07-21 23:59:07 +0300

    debug: show saved task state in cloud status on phone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'relocation-v6.37.3-98b6c04';
+const CACHE_NAME = 'relocation-v6.37.4-e653919';
 const FILES = [
   './', './index.html', './style.css', './app.js', './data.js', './sync.js',
   './manifest.json', './icon.svg',
